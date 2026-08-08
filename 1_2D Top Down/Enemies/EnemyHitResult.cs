bd7e1083ffab2b83e20f817edf4563930d60a870
ivan pavlov <Evilbxnny@gmail.com>
2026-08-09 01:53:33 +0300

    Merge commit

---
+++ 1_2D Top Down/Enemies/EnemyHitResult.cs
@@ -0,0 +1,30 @@
+﻿namespace _1_2D_Top_Down
+{
+    /// <summary>
+    /// Describes the result of one player projectile hitting an enemy.
+    /// Game1 uses this to apply mission-wide effects such as XP, drops,
+    /// sounds and visual death animations.
+    /// </summary>
+    public readonly struct EnemyHitResult
+    {
+        public Enemy? Enemy { get; }
+        public EnemyType? DefeatedEnemyType { get; }
+
+        public bool HasHit => Enemy != null;
+        public bool HasDefeatedEnemy => DefeatedEnemyType != null;
+
+        private EnemyHitResult(Enemy? enemy, EnemyType? defeatedEnemyType)
+        {
+            Enemy = enemy;
+            DefeatedEnemyType = defeatedEnemyType;
+        }
+
+        public static EnemyHitResult Miss() => new(null, null);
+
+        public static EnemyHitResult Hit(Enemy enemy) => new(enemy, null);
+
+        public static EnemyHitResult Defeat(
+            Enemy enemy,
+            EnemyType enemyType) => new(enemy, enemyType);
+    }
+}
